381476cd577b8b56ab518694096826d89bd8d5fa
Leonardo Rodriguez Di Stefano <leord.distefano@gmail.com>
2025-10-15 17:47:15 +0200

    Add nodemailer type declarations

---
+++ types/nodemailer.d.ts
@@ -0,0 +1,28 @@
+declare module 'nodemailer' {
+  export interface TransportOptions {
+    host?: string;
+    port?: number;
+    secure?: boolean;
+    auth?: {
+      user: string;
+      pass: string;
+    };
+  }
+
+  export interface MailOptions {
+    from: string;
+    to: string;
+    replyTo?: string;
+    subject: string;
+    text: string;
+  }
+
+  export interface Transporter {
+    sendMail(mailOptions: MailOptions): Promise<any>;
+  }
+
+  export function createTransport(options: TransportOptions): Transporter;
+  
+  export default { createTransport };
+}
+
